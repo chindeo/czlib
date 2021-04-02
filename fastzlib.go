c694f4f07f600b6fd2f0269b0a969b0f5553bf8a
snowlyg <luodan@zhimadi.cn>
2021-04-02 21:15:19 +0800

    add funcc

--- fastzlib.go
+++ fastzlib.go
@@ -3,7 +3,10 @@
 package czlib
 
 import (
+	"bytes"
 	"errors"
+	"fmt"
+	"io/ioutil"
 	"unsafe"
 )
 
@@ -119,4 +122,36 @@ func UnsafeCompress(input []byte) (UnsafeByte, error) {
 
 	b := NewUnsafeByte((*C.char)(ret.str), int(ret.len))
 	return b, nil
+}
+
+// 相关文档 http://www.zlib.net/manual.html#Basic
+
+func Gzip(body []byte) ([]byte, error) {
+	outb := make([]byte, 0, 16*1024)
+	out := bytes.NewBuffer(outb)
+	writer, err := czlib.NewWriterLevel2(out, -1, 8, -9, 8, 0)
+	if err != nil {
+		return []byte{}, err
+	}
+	n, err := writer.Write(body)
+	if n != len(body) {
+		return []byte{}, fmt.Errorf("compressed %d, expected %d", n, len(body))
+	}
+	if err != nil {
+		return []byte{}, err
+	}
+	err = writer.Close()
+	if err != nil {
+		return []byte{}, err
+	}
+	return out.Bytes(), nil
+}
+
+// 使用参考 https://github.com/madler/zlib/blob/master/examples/zran.c
+func Gunzip(body []byte) ([]byte, error) {
+	reader, err := czlib.NewReaderLevel2(bytes.NewBuffer(body), -15)
+	if err != nil {
+		return []byte{}, err
+	}
+	return ioutil.ReadAll(reader)
 }
